cd20f46a4d643c87288c4a2feaea2d78abc751f0
CodderG <127430228+ginerJ@users.noreply.github.com>
2025-09-17 13:24:26 -0300

    🦮 Tameable Beast Wiki Mob Lore

---
+++ js/texts/tameable_beasts.js
@@ -0,0 +1,35 @@
+document.getElementById("racoon-lore").innerHTML = `
+    <p class="font-bold text-2xl">
+      <span class="text-special">RACOON</span>: Spawn on 
+      <span class="text-orange">Forests</span>, 
+      <span class="text-orange">Snowy Taigas</span> 
+      and <span class="text-orange">Bamboo Jungles</span>. 
+      They are mischievous creatures that will steal edible items from your hands 
+      and pollen from bees. After eating the looted nutrients they will become fat 
+      and avoid the player, dropping <span class="text-orange">Racoon Fur</span> 
+      before deflating. They can be tamed with <span class="text-orange">Melon Slices</span> 
+      and bred with <span class="text-orange">Chiken Eggs</span>. 
+      A happy racoon will drop <span class="text-orange">Racoon Fur</span> from brushing.
+    </p>
+  `;
+
+document.getElementById("penguin-lore").innerHTML = `
+  <p class="font-bold text-2xl">
+    <span class="text-special">PENGUIN</span>: Spawn on <span class="text-orange">Stony Shores</span>, 
+    <span class="text-orange">Frozen Oceans</span>, <span class="text-orange">Snowy Beaches</span>, frozen 
+    peaks and beaches. They are hungry for war and can be equiped with <span class="text-orange">Icepops</span>
+     and <span class="text-orange">Ice Armor</span>. They can be tamed
+     with <span class="text-orange">Frozen Fish</span> and bred with any fish item. A happy penguin will drop 
+     <span class="text-orange">Feathers</span> from brushing.
+  </p>
+`;
+
+document.getElementById("chikote-lore").innerHTML = `
+  <p class="font-bold text-2xl">
+    <span class="text-special">Chikote</span>: Spawn on <span class="text-orange">Deserts</span>, <span class="text-orange">Plains</span> and
+     <span class="text-orange">Snowy Plains</span>. They are definitely
+     not chocobos and will allow you to ride them, having the ability of slow fall, beware though, untamed chikotes will try to
+     loot your crops. They can be tamed with <span class="text-orange">Potatoes</span> and <span class="text-orange">Big Bird Bait</span> and bred with 
+    <span class="text-orange">Beetroot</span>. A happy chikote will drop Feathers from brushing.
+  </p>
+`;
